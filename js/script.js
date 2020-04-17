7ef18e26ddf11f5b6ba2562f84ed93679bfc9e6e
fumiya-ozaki <fumiya0414@gmail.com>
2020-04-17 11:30:52 +0900

    フロントページマークアップ

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ var mySwiper = new Swiper ('.slide1', {
   speed:4000, //スクロールスピード小さいほど早い
   // direction: 'vertical', //スクロール方向 縦
   // spaceBetween: 1000, //写真の間の余白
-  // pagination
+
+  // pagination //
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
@@ -25,6 +26,7 @@ var mySwiper = new Swiper ('.slide1', {
   },
 })
 
+//textillate
 $('#mv-title').textillate({
   loop:true,
   in:{
@@ -40,6 +42,7 @@ $('#mv-title').textillate({
 
 });
 
+//swiper
 var swiper = new Swiper('.slide2', {
   effect: 'coverflow',
   grabCursor: true,
@@ -61,4 +64,58 @@ var swiper = new Swiper('.slide2', {
   },
 });
 
+  //scrollmagic
+  var controller = new ScrollMagic.Controller(); //先頭にかならずつける
+  //parallax scene
+  var parallaxTL =new TimelineMax();
+  parallaxTL
+    .from('.contents__wrapper', 2,{opacity: 0, delay:4})
+    .from('.bcg',10,{y: '-50%', ease:Power0.easeNone},0)
+    ;    
+  var slideParallaxScene = new ScrollMagic.Scene({
+    triggerElement: '.bcg-parallax',
+    triggerHook:1,
+    duration:'150%'
+  })
+  .addIndicators({
+    name: 'parallax', //indicaterの名前
+    colorTrigger: 'black', //triggerの色
+    colorStart: '#75C695', //開始箇所の色
+    colorEnd:'pink' //終了箇所の色
+  })
+  .setTween(parallaxTL)
+  .addTo(controller); //コントローラー（必ずつける）
+
+  //profile部分
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement:".profile",
+    reverse: false,
+  })
+  .setTween(TweenMax.from('.profile__text', 2,{opacity: 0, delay:0}))
+  .addIndicators({
+    name: 'profile', //indicaterの名前
+    colorTrigger: 'black', //triggerの色
+    colorStart: '#75C695', //開始箇所の色
+    colorEnd:'pink' //終了箇所の色
+  })
+  
+  .addTo(controller); //コントローラー（必ずつける）
+
+  //product部分
+  $('.fadeIn').each(function(){
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this, //イベント開始場所 入れ子可能
+      duration: '110%', //イベント終了場所 要素に対して
+      // triggerHook: 0.9, //開始場所変更 0はページトップ 
+      // reverse: false, //イベント繰り返し falseは１回だけ durationを消すと良い
+    })
+    .setClassToggle(".item","fade-in")
+    .addIndicators({
+      name: 'fade-in', //indicaterの名前
+      colorTrigger: 'black', //triggerの色
+      colorStart: '#75C695', //開始箇所の色
+      colorEnd:'pink' //終了箇所の色
+    })
+    .addTo(controller); //コントローラー（必ずつける）
+  });
 });
